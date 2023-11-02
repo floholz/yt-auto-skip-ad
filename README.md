d012168e2dfb4c78421c197ff6869f7d62dc3042
floholz <holzmann96flo@gmail.com>
2023-11-02 02:16:18 +0100

    > implemented base functionality of extension

--- README.md
+++ README.md
@@ -1,20 +1,6 @@
-# Chrome Extension V3 - Template
+# YT auto skip Ads
 
-A starter project for developing a simple [Chrome Extension](https://developer.chrome.com/docs/extensions/) using Manifest V3. 
+A Google Chrome extension, that automatically skips ads if possible :)
 
-## Content
-- Manifest V3
-- Popup page
-- Background script
-- content script
 
-## Usage
-![use-this-template-button.webp](https://docs.github.com/assets/cb-77734/mw-1440/images/help/repository/use-this-template-button.webp)
-1. Above the code section, click the **Use this template** button
-2. Select Create a new repository
-3. Configure your repository 
-   - Provide a **name**
-   - Choose a **visibility** 
-   - (optional) Add a description
-4. Click **Create repository from template**
 
--- content.js
+++ content.js
@@ -1,1 +1,72 @@
-document.body.style.backgroundColor = '#ae09e3';
+const _ = "EXT_YT_SKIP_ADD";
+console.log(_, 'injected');
+
+
+
+/* inject skip notification */
+const notificationElement = document.createElement('div');
+notificationElement.id = 'ext-yt-ad-skip_notification';
+notificationElement.innerHTML = 'Ad Skipped :)';
+
+const notificationContainerElement = document.createElement('div');
+notificationContainerElement.id = 'ext-yt-ad-skip_notification-container';
+notificationContainerElement.append(notificationElement);
+
+document.body.append(notificationContainerElement);
+
+
+
+
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callback = (mutationList, observer) => {
+    for (const mutation of mutationList) {
+        if (mutation.type === "childList" || mutation.type === "attributes") {
+            console.log(_, 'mutation.type', mutation.type);
+            checkSkipButton();
+        }
+    }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+const observeInterval = setInterval(() => {
+
+    try {
+        // Select the node that will be observed for mutations
+        const targetNode = document.querySelectorAll('.ytp-ad-module')[0];
+        observer.observe(targetNode, config);
+        clearInterval(observeInterval);
+    } catch (e) {
+        console.log(e);
+        console.log(_, 'failed to observe, try again ..');
+    }
+}, 1000);
+
+// Later, you can stop observing
+// observer.disconnect();
+
+
+function checkSkipButton() {
+    console.log(_, 'Ad check');
+    const skipBtn = document.querySelectorAll('.ytp-ad-skip-button')[0];
+    const skipSlot = document.querySelectorAll('.ytp-ad-skip-button-slot')[0];
+
+    if (skipBtn && skipSlot.style.display !== 'none') {
+        skipBtn.click();
+        showNotification();
+        console.log(_, 'skipped Ad');
+    }
+}
+
+function showNotification(duration = 5000) {
+    notificationContainerElement.classList.add('active');
+    setTimeout(() => {
+        notificationContainerElement.classList.remove('active');
+    }, duration);
+}
